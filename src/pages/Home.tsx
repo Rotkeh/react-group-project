fa4125c396fbc3aebf7746b67ff10e936b3110f8
Rotkeh <hektordavidsson@gmail.com>
2024-09-09 14:20:54 +0200

    home fix

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,31 +1,29 @@
 import { useEffect, useState } from "react";
 
-
-const API_URL = "www.thecocktaildb.com/api/json/v1/1/random.php";
-
+const API_URL = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
 
 export function Home() {
-
-  const [cocktail, setCocktail] = useState<[]>([])
+  const [cocktail, setCocktail] = useState<[]>([]);
 
   useEffect(() => {
     const fetchCocktail = async () => {
       try {
         const response = await fetch(API_URL);
-        debugger;
         console.log("API key fetch OK");
         const data = await response.json();
         console.log(data);
         setCocktail(data.drinks[0]);
-
       } catch (error) {
         console.log(error);
       }
-    }
+    };
     fetchCocktail();
   }, []);
 
-  return <div><h1>Home</h1>
-    <h2>{cocktail.strDrink}</h2>
-  </div>;
+  return (
+    <div>
+      <h1>Home</h1>
+      <h2>{cocktail.strDrink}</h2>
+    </div>
+  );
 }
