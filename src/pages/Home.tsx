b441b662586bc15f52dd93ce7a504832c3054229
Michael Hutt <michael_w_hutt@hotmail.com>
2024-09-09 14:11:19 +0200

    Home page fetch almost OK

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,31 @@
-API_URL = "";
+import { useEffect, useState } from "react";
+
+
+const API_URL = "www.thecocktaildb.com/api/json/v1/1/random.php";
+
 
 export function Home() {
-  return <div>Home</div>;
+
+  const [cocktail, setCocktail] = useState<[]>([])
+
+  useEffect(() => {
+    const fetchCocktail = async () => {
+      try {
+        const response = await fetch(API_URL);
+        debugger;
+        console.log("API key fetch OK");
+        const data = await response.json();
+        console.log(data);
+        setCocktail(data.drinks[0]);
+
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchCocktail();
+  }, []);
+
+  return <div><h1>Home</h1>
+    <h2>{cocktail.strDrink}</h2>
+  </div>;
 }
