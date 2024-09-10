ddc2d7396c10fb79db471c98994e44a4489d8e38
Jessica Hansson <jessica.hansson@hotmail.se>
2024-09-10 11:07:40 +0200

    component cocktail-card working

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,54 +1,14 @@
-import { useEffect, useState } from "react";
-import { ICocktail } from "../interface";
 import { Link } from "react-router-dom";
-
-const API_URL = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
-
+import { CocktailCard } from "../components/CocktailCard";
 
 export function Home() {
-  const [cocktail, setCocktail] = useState<ICocktail>();
-
-  useEffect(() => {
-    const fetchCocktail = async () => {
-      try {
-        const response = await fetch(API_URL);
-        const data = await response.json();
-        setCocktail(data.drinks[0]);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchCocktail();
-  }, []);
-
   return (
-    <main className="body">
-      {cocktail && (
-        <>
-          <h1>{cocktail.strDrink}</h1>
-          <h4>{cocktail.strCategory}</h4>
-          <p>{cocktail.strInstructions}</p>
-          <Link to ="/search">Search</Link>
-        </>
-      )}
-    </main>
+    <>
+      <main className="body">
+        <CocktailCard />
+        <Link to="/search">Search</Link>
+      </main>
+    </>
   );
 }
 
-// import { useState, useEffect } from "react";
-// import "./App.css";
-
-// function App() {
-//   let [randomCocktail, setRandomCocktail] = useState(null);
-
-//   useEffect(() => {
-//     fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
-//       .then((response) => response.json())
-//       .then((data) => setRandomCocktail(data));
-//     console.log(randomCocktail);
-//   }, []);
-
-//   return <div className="App"></div>;
-// }
-
-// export default App;
