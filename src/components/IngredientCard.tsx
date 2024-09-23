82ec880ec59b30913e51807ba518d368b35cfd27
Jessica Hansson <jessica.hansson@hotmail.se>
2024-09-23 15:56:20 +0200

    styling of ingredient page

--- src/components/IngredientCard.tsx
+++ src/components/IngredientCard.tsx
@@ -19,23 +19,25 @@ export function IngredientCard({ ingredient, img, cocktails }: IngredientProps) 
     <section className="ingredient-container">
       <section className="drink-info">
         <img src={img} className="ingredient-img" alt={ingredient.strIngredient} />
-        <h1>{ingredient.strIngredient}</h1>
-        {ingredient.strType ? (
+        <h1 className="ingredient-title">{ingredient.strIngredient}</h1>
+        <article className="ingredient-tags">
+          {ingredient.strType ? (
+            <p className="category">
+              <strong>Type:</strong> {ingredient.strType}
+            </p>
+          ) : (
+            ""
+          )}
           <p className="category">
-            <strong>Type:</strong> {ingredient.strType}
+            <strong>Alcoholic:</strong> {ingredient.strAlcohol}
           </p>
-        ) : (
-          ""
-        )}
-        <p className="category">
-          <strong>Alcoholic:</strong> {ingredient.strAlcohol}
-        </p>
-        <p className="category">
-          <strong>Alcoholic content:</strong> {ingredient.strABV ? ingredient.strABV : "0"}%
-        </p>
+          <p className="category">
+            <strong>Alcoholic content:</strong> {ingredient.strABV ? ingredient.strABV : "0"}%
+          </p>
+        </article>
         <p className="ingredient-description">{ingredient.strDescription}</p>
       </section>
-      <h3>Drinks containing {ingredient.strIngredient}</h3>
+      <h3>Drinks containing {ingredient.strIngredient}:</h3>
       <button onClick={() => setShowAll((prev) => !prev)}>
         {showAll ? "Show less" : "Show all drinks"}
       </button>
@@ -55,6 +57,3 @@ export function IngredientCard({ ingredient, img, cocktails }: IngredientProps) 
     </section>
   );
 }
-
-// Användare ska kunna se detaljerad information om en ingrediens genom att klicka på den.
-//Informationen ska inkludera namn, beskrivning, alkoholhalt (om tillämpligt), och vilka andra cocktails som innehåller den.
