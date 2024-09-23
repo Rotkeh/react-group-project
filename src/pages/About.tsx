4dbad45a585a60d429351aae853250cd79d780b0
Michael Hutt <michael_w_hutt@hotmail.com>
2024-09-23 10:07:43 +0200

    about page fixad igen

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,26 +2,26 @@ export function About() {
     return (
         <>
             <main className = "body">
-                <h3>More info about Landing page</h3>
+                <h3>Landing page</h3>
                 <p id="landingPageInfo">This page presents a random drink, shown in a card.
                     To change to a new random drink, press the button "Get me a new drink!"
                     If you want to add the drink to your favorite list, click on the heart symbol.
                     If you want to see more information about a specific drink, click anywhere inside the drink card.
                 </p>
-                <h3>More info about Search Page</h3>
+                <h3>Search Page</h3>
                 <p>This page lets you search for a drink.
                     The search can be made in several different ways.
                     1. Enter a part of the name of the drink into the input field, if you know the name of the drink you are searching for.
                     2. Choose between category - glass - ingredient if you want a more organized search.
                     If you want to see more information about a specific drink, click anywhere inside the drink card.
                 </p>
-                <h3>More info about Cocktailinfo Page</h3>
+                <h3>Cocktailinfo Page</h3>
                 <p>
                     In this page you can find extended information about a specific drink.
                     You can find a list of ingredients, category of drink, type of glass.
                     Also the drink is listed as Alcoholic/Non alcoholic
                 </p>
-                <h3>More info about Favorite Page</h3>
+                <h3>Favorite Page</h3>
                 <p>
                     This page shows your created list of favorite drinks. 
                     You can choose to remove drinks from the list by clinking on the heart symbol.
